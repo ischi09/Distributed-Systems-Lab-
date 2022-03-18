c48b0a7aa3ef8435823b54ad5b579142a413ad49
ischi09 <ishaan.shamanna@gmail.com>
2022-03-18 14:17:22 +0100

    Added config file and python config

---
+++ src/deepsets/config.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Paths:
+    log: str
+    results: str
+
+@dataclass
+class Experiment:
+    epochs: int
+    lr: float
+    batch_size: int
+    random_seed: int
+    loss: str
+    weight_decay: float
+
+
+@dataclass
+class Model:
+  accumulator: str
+  laten_dim: int
+
+
+@dataclass
+class Trainset:
+  n_samples: int
+  max_set_size: int
+  min_value: int
+  max_value: int
+  label: str
+  multisets: bool
+
+@dataclass
+class Validset:
+  n_samples: int
+  max_set_size: int
+  min_value: int
+  max_value: int
+  label: str
+  multisets: bool
+
+@dataclass
+class Testset:
+  n_samples: int
+  max_set_size: int
+  min_value: int
+  max_value: int
+  label: str
+  multisets: bool
