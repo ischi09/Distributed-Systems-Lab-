6d756edd6cd504fe589e12ed0904a1619224271c
ischi09 <ishaan.shamanna@gmail.com>
2022-03-21 10:46:07 +0100

    Added Generate Dataset Function with config files

--- src/deepsets/datasets.py
+++ src/deepsets/datasets.py
@@ -1,11 +1,26 @@
 from typing import Callable, Any
 import random
 
+from isort import Config
+import hydra
+
 import numpy as np
 import torch
 from torch.utils.data.dataset import Dataset
 
+# For soem reasone the 
+def generateDataset(cfg:Config, label_generator:Callable[[Any],Any]):
+    train = cfg.trainset
+    valid = cfg.validset
+    test = cfg.testset
 
+    train_set = SetDataset(train.n_samples, train.max_set_size, train.min_value, train.max_value, label_generator, train.multisets)
+    valid_set = SetDataset(valid.n_samples, valid.max_set_size, valid.min_value, valid.max_value, label_generator, valid.multisets)
+    test_set = SetDataset(test.n_samples, test.max_set_size, test.min_value, test.max_value, label_generator, test.multisets)
+
+    return train_set, valid_set, test_set
+
+    
 class SetDataset(Dataset):
     def __init__(
             self, n_samples: int, max_set_size: int, min_value: int,
