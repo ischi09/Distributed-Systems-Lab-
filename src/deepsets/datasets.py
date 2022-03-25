55348e1e26e06104f1048d6451ce274efe2fc6e3
ischi09 <ishaan.shamanna@gmail.com>
2022-03-25 17:17:35 +0100

    Liniting changes

--- src/deepsets/datasets.py
+++ src/deepsets/datasets.py
@@ -22,26 +22,59 @@ def get_cardinality(x: torch.Tensor):
 def get_sum(x: torch.Tensor):
     return x.sum()
 
-LABEL_GENERATORS = {"sum": get_sum, "cardinality": get_cardinality, "mode": get_mode, "max": get_max}
 
-def generate_datasets(cfg:Config):
+
+LABEL_GENERATORS = {
+    "sum": get_sum,
+    "cardinality": get_cardinality,
+    "mode": get_mode,
+    "max": get_max,
+}
+
+
+def generate_datasets(cfg: Config):
     train = cfg.trainset
     valid = cfg.validset
     test = cfg.testset
 
-    train_set = SetDataset(train.n_samples, train.max_set_size, train.min_value, train.max_value, LABEL_GENERATORS[train.label], train.multisets)
-    valid_set = SetDataset(valid.n_samples, valid.max_set_size, valid.min_value, valid.max_value, LABEL_GENERATORS[valid.label], valid.multisets)
-    test_set = SetDataset(test.n_samples, test.max_set_size, test.min_value, test.max_value, LABEL_GENERATORS[test.label], test.multisets)
+    train_set = SetDataset(
+        train.n_samples,
+        train.max_set_size,
+        train.min_value,
+        train.max_value,
+        LABEL_GENERATORS[train.label],
+        train.multisets,
+    )
+    valid_set = SetDataset(
+        valid.n_samples,
+        valid.max_set_size,
+        valid.min_value,
+        valid.max_value,
+        LABEL_GENERATORS[valid.label],
+        valid.multisets,
+    )
+    test_set = SetDataset(
+        test.n_samples,
+        test.max_set_size,
+        test.min_value,
+        test.max_value,
+        LABEL_GENERATORS[test.label],
+        test.multisets,
+    )
 
     return train_set, valid_set, test_set
 
 
 class SetDataset(Dataset):
     def __init__(
-            self, n_samples: int, max_set_size: int, min_value: int,
-            max_value: int, label_generator: Callable[[Any],
-                                                      Any],
-            generate_multisets=False):
+        self,
+        n_samples: int,
+        max_set_size: int,
+        min_value: int,
+        max_value: int,
+        label_generator: Callable[[Any], Any],
+        generate_multisets=False,
+    ):
         self.n_samples = n_samples
         self.label_generator = label_generator
         self.items = []
@@ -50,9 +83,8 @@ class SetDataset(Dataset):
             values = np.arange(min_value, max_value)
             rand_set_size = random.randint(1, max_set_size)
             rand_set = np.random.choice(
-                values,
-                (rand_set_size, 1),
-                replace=generate_multisets)
+                values, (rand_set_size, 1), replace=generate_multisets
+            )
             item = torch.tensor(rand_set, dtype=torch.float)
             self.items.append(item)
 
--- src/deepsets/experiments.py
+++ src/deepsets/experiments.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from torch import optim
 from tqdm import tqdm
 
