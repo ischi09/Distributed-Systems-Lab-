f788d120271904cfd35f0838522df75d487520ea
smueksch <sebastian.mueksch+github@gmail.com>
2022-03-28 01:22:13 +0200

    Adding batched accumulators for mean, std, max and min.

--- src/deepsets/networks.py
+++ src/deepsets/networks.py
@@ -6,19 +6,44 @@ import torch.nn as nn
 from .config import Model as ModelConfig
 
 
-def accumulate_sum(x: torch.FloatTensor) -> torch.FloatTensor:
+def accumulate_sum(x: torch.FloatTensor, mask: torch.FloatTensor) -> torch.FloatTensor:
+    x = x * mask
     return x.sum(axis=1)
 
 
-def accumulate_max(x: torch.FloatTensor) -> torch.FloatTensor:
+def accumulate_mean(x: torch.FloatTensor, mask: torch.FloatTensor) -> torch.FloatTensor:
+    sum = accumulate_sum(x, mask)
+    n_elements = mask.sum(axis=1)
+    return sum / n_elements
+
+
+def accumulate_std(x: torch.FloatTensor, mask: torch.FloatTensor) -> torch.FloatTensor:
+    mean = accumulate_mean(x, mask).unsqueeze(dim=-1)
+    variance = accumulate_mean(torch.square(x - mean), mask)
+    return torch.sqrt(variance)
+
+
+def accumulate_max(x: torch.FloatTensor, mask: torch.FloatTensor) -> torch.FloatTensor:
+    neg_infinity = torch.tensor(float("-inf"))
+    x = torch.where(mask.byte(), x, neg_infinity)
     return x.max(dim=1).values
+
+
+def accumulate_min(x: torch.FloatTensor, mask: torch.FloatTensor) -> torch.FloatTensor:
+    return -accumulate_max(-x, mask)
 
 
 def count_parameters(model: nn.Module) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-ACCUMLATORS = {"sum": accumulate_sum, "max": accumulate_max}
+ACCUMLATORS = {
+    "sum": accumulate_sum,
+    "mean": accumulate_mean,
+    "std": accumulate_std,
+    "max": accumulate_max,
+    "min": accumulate_min,
+}
 
 
 def generate_model(config: ModelConfig) -> nn.Module:
@@ -45,7 +70,9 @@ class DeepSetsInvariant(nn.Module):
         self,
         phi: nn.Module,
         rho: nn.Module,
-        accumulator: Callable[[torch.FloatTensor], torch.FloatTensor],
+        accumulator: Callable[
+            [torch.FloatTensor, torch.FloatTensor], torch.FloatTensor
+        ],
     ):
         super().__init__()
         self.phi = phi
@@ -56,8 +83,7 @@ class DeepSetsInvariant(nn.Module):
         self, x: torch.FloatTensor, mask: torch.FloatTensor
     ) -> torch.FloatTensor:
         x = self.phi(x)  # x.shape = (batch_size, max_set_size, input_dim)
-        x = x * mask
-        x = self.accumulator(x)
+        x = self.accumulator(x, mask)
         return self.rho(x)
 
 
