301f81146b89ab7524b73e3d283df9f3d1afb292
smueksch <sebastian.mueksch+github@gmail.com>
2022-04-05 12:37:57 +0200

    Adding dataset summary statistic logging to terminal.

--- src/run.py
+++ src/run.py
@@ -22,6 +22,14 @@ def main(config: Config):
 
     train_set, valid_set, test_set = generate_datasets(config)
 
+    print("Training Dataset Statistics:")
+    print(f"Label mean: {train_set.get_label_mean()}")
+    print(f"Label std: {train_set.get_label_std()}")
+    print(f"Label median: {train_set.get_label_median()}")
+    print(f"Label mode: {train_set.get_label_mode()}")
+    print(f"Label max: {train_set.get_label_max()}")
+    print(f"Label min: {train_set.get_label_min()}\n")
+
     model = generate_model(config.model, delta=train_set.get_delta())
 
     experiment = Experiment(
