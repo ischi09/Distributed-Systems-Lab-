81513f4a7a45e41c8abfa6db94c459829094e04e
ischi09 <ishaan.shamanna@gmail.com>
2022-05-22 21:31:59 +0200

    Added contains_even to the sweeps

--- sweeps/pna-diff-task-sizes.sh
+++ sweeps/pna-diff-task-sizes.sh
@@ -11,7 +11,7 @@ python run.py -m \
 experiment.random_seed=9,99,999,9999,99999 \
 experiment.results_out=pna-diff-task-sizes-results.csv \
 experiment.use_gpu=True \
-task.label=mode,longest_seq_length,largest_contiguous_sum,max,cardinality \
+task.label=mode,longest_seq_length,largest_contiguous_sum,max,cardinality,contains_even \
 task.max_set_size=8,16,32,64,128,256,512,1024 \
 task.multisets=True \
 model.type=pna
